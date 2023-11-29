f69923e52b963a191b01cec9cd8b3832302946ad
aprilepskiy <centurion8484@gmail.com>
2023-11-29 13:25:17 +0200

    home work 19 refactoring

--- src/test/java/homeWork19/BaseUITest.java
+++ src/test/java/homeWork19/BaseUITest.java
@@ -5,16 +5,21 @@ import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 
 public class BaseUITest {
     public WebDriver driver;
     public TestProperties props;
 
+    @BeforeClass
+    public void setUpClass() {
+        props = new TestProperties();
+    }
+
     @BeforeMethod
     public void setup() {
         WebDriverManager.chromedriver().setup();
-        props = new TestProperties();
         driver = new ChromeDriver();
         driver.manage().window().setSize(new Dimension(1920, 1080));
         driver.get(props.getProperty("url"));
